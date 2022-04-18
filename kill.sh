1a6e03fc0a602bfb92399099fa6b9ac0834dd3fe
Coyls <loic.grecofr@gmail.com>
2022-04-19 00:18:04 +0200

    sudo on kill.sh

--- kill.sh
+++ kill.sh
@@ -3,9 +3,9 @@
 # Execute POC
 
 cd /home/pi/Documents/poc
-pkill -f server.py 
-pkill -f tmp.py 
-pkill -f proximity.py 
-pkill -f rotary.py 
-pkill -f ground-humidity.py &
+sudo pkill -f server.py 
+sudo pkill -f tmp.py 
+sudo pkill -f proximity.py 
+sudo pkill -f rotary.py 
+sudo pkill -f ground-humidity.py &
 sudo pkill -f switch.py 
